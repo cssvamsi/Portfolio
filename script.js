428314e08907af2d24b0776663a209a160c5428d
cssvamsi <cssvamsi@gmail.com>
2017-03-10 17:43:17 -0600

    First Website

---
+++ script.js
@@ -0,0 +1,18 @@
+/*Animation script*/
+
+$(document).ready(function(){
+      $('.nav li a').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+          var $target = $(this.hash);
+          $target = $target.length && $target
+          || $('[name=' + this.hash.slice(1) +']');
+          if ($target.length) {
+            var targetOffset = $target.offset().top;
+            $('html,body')
+            .animate({scrollTop: targetOffset}, 1500);
+           return false;
+          }
+        }
+      });
+    });
